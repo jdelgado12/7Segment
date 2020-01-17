2e14d55bdc721a5eab736f0e7fbca50bd493c29b
jdelgado12 <iamidumz12@gmail.com>
2020-01-17 19:05:32 +0800

    Baka gumana na

--- 7Segment/Form1.vb
+++ 7Segment/Form1.vb
@@ -1,27 +1,37 @@
 ﻿Public Class Form1
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs)
-        If Timer1.Interval = 0 Then
-            Button1.ForeColor = Color.Red
-            Button2.BackColor = Color.Red
-            Button3.BackColor = Color.Red
-            Button4.BackColor = Color.Gray
-            Button5.BackColor = Color.Red
-            Button6.BackColor = Color.Red
-            Button7.BackColor = Color.Red
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        If Timer1.Interval = 1000 Then
+            Button1.Visible = True
+            Button2.Visible = True
+            Button3.Visible = True
+            Button4.Visible = False
+            Button5.Visible = True
+            Button6.Visible = True
+            Button7.Visible = True
         ElseIf Timer1.Interval = 2000 Then
-            Button1.BackColor = Color.Red
-            Button2.BackColor = Color.Red
-            Button3.BackColor = Color.Red
-            Button4.BackColor = Color.Red
-            Button5.BackColor = Color.Red
-            Button6.BackColor = Color.Red
-            Button7.BackColor = Color.Red
+            Button1.Visible = False
+            Button2.Visible = True
+            Button3.Visible = False
+            Button4.Visible = False
+            Button5.Visible = True
+            Button6.Visible = False
+            Button7.Visible = False
 
         End If
 
 
 
 
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Timer1_Tick_1(sender As Object, e As EventArgs) Handles Timer1.Tick
 
     End Sub
 End Class
